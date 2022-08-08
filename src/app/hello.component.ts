258512ca8b5e86e1c7f5a309e6f5a20db87118f2
kravindra1415 <kravindra1415@gmail.com>
2022-08-08 14:12:13 +0530

    FormBuilder without DI (commented)

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -4,7 +4,6 @@ import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-hello',
   templateUrl: './hello.component.html',
-  //styles: [`h1 { font-family: Lato; }`],
   styleUrls: ['./hello.component.css'],
 })
 export class HelloComponent {
@@ -16,33 +15,39 @@ export class HelloComponent {
 
   // using Form Group
   //----------------------------------------------------
-  customerForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    address: new FormGroup({
-      street: new FormControl(''),
-      city: new FormControl(''),
-      state: new FormControl(''),
-      zipcode: new FormControl(''),
-    }),
-    mobile: new FormControl(''),
-  });
+  // customerForm = new FormGroup({
+  //   firstName: new FormControl(''),
+  //   lastName: new FormControl(''),
+  //   address: new FormGroup({
+  //     street: new FormControl(''),
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     zipcode: new FormControl(''),
+  //   }),
+  //   mobile: new FormControl(''),
+  // });
   //------------------------------------------------------
 
-  // constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) {}
 
-  // //using FormBuilder
-  // customerForm = this.fb.group({
-  //   firstName: [''],
-  //   lastName: [''],
-  //   address: this.fb.group({
-  //     street: [''],
-  //     city: [''],
-  //     state: [''],
-  //     zipcode: [],
-  //   }),
-  //   mobile: [''],
+  //formBuilder without DI
+  // customerForm = new FormBuilder().group({
+  //   a: [],
+  //   b: [],
   // });
+
+  //using FormBuilder
+  customerForm = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zipcode: [],
+    }),
+    mobile: [''],
+  });
 
   saveCustomer() {
     console.log(this.customerForm.value);
